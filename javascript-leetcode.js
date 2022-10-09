0aa27f64f2e951b95c8dd1ae0b0a36f4a89871ad
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-08 23:59:11 -0500

    finished twosum question

--- javascript-leetcode.js
+++ javascript-leetcode.js
@@ -148,3 +148,17 @@ let longestCommonPrefix = function(strs) {
 // 	-109 <= nums[i] <= 109
 // 	-109 <= target <= 109
 // Only one valid answer exists.
+
+var twoSum = function(nums, target) {
+	const hmap = {}
+	for (var idx=0; idx<nums.length; idx++) {
+		var val = nums[idx]
+		var delta = target - val
+		var deltaIdx = hmap[delta]
+		if (deltaIdx !== undefined && deltaIdx !== idx) {
+			return [deltaIdx, idx]
+		}
+		hmap[val] = idx
+	}
+	return []
+};
