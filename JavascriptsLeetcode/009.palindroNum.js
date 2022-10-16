0ac06aad87163138f55f084a89c5b202f4eb6c31
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-15 23:33:12 -0500

    added palindrome solution

--- JavascriptsLeetcode/009.palindroNum.js
+++ JavascriptsLeetcode/009.palindroNum.js
@@ -26,3 +26,13 @@
 //
 // -231 <= x <= 231 - 1
 
+var isPalindrome = function(x) {
+	if (x < 0) return false;
+	var revX = 0;
+	var y = x;
+	while (x > 0) {
+		revX = revX * 10 + x % 10;
+		x = Math.floor(x / 10);
+	}
+	return y === revX;
+};
