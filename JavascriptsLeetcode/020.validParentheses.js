48c9fe4868196850e006f702eb6d36ba97569ab0
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-10-31 21:08:58 -0500

    added solution

--- JavascriptsLeetcode/020.validParentheses.js
+++ JavascriptsLeetcode/020.validParentheses.js
@@ -26,3 +26,24 @@
 // 	1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
 
+const isValid = (s) => {
+	if (s.length % 2 !== 0) return false;
+
+	const stack = [];
+	const map = new Map([
+		['(', ')'],
+		['[', ']'],
+		['{', '}']
+	]);
+
+	for (let i = 0 ; i < s.length ; i += 1) {
+		if (map.has(s[i])) {
+			stack.push(map.get(s[i]));
+		} else if (s[i] !== stack.pop()) {
+			return false;
+		}
+	}
+	return stack.length === 0;
+};
+
+
