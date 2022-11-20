e9fbce744c9eeaaeaf33399ed6f627ebe5f3feaa
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-19 23:34:13 -0600

    added leetcode solution

--- JavascriptsLeetcode/026.removeDuplicatesfromSortedArray.js
+++ JavascriptsLeetcode/026.removeDuplicatesfromSortedArray.js
@@ -43,5 +43,22 @@
 // 	-100 <= nums[i] <= 100
 // nums is sorted in non-decreasing order.
 
+let removeDuplicates = function(nums) {
+	if(nums.length == 0) return 0;
 
+	let i = 0;
+	for (let j = 1; j < nums.length; j ++){
+		if(nums[j] !== nums[i]){
+			i++;
+			nums[i] = nums[j];
+		}
+	}
+
+	return i + 1
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 
