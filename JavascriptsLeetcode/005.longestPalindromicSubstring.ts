50fac02fd801377cbe48c93d111a972d3afa0136
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-16 23:59:53 -0600

    adding q

---
+++ JavascriptsLeetcode/005.longestPalindromicSubstring.ts
@@ -0,0 +1,32 @@
+// Given a string s, return the longest
+// palindromic
+//
+// substring
+// in s.
+
+//     Example 1:
+//
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+//     Example 2:
+//
+// Input: s = "cbbd"
+// Output: "bb"
+//
+//
+// Constraints:
+//
+//     1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+//returns string
+// https://en.wikipedia.org/wiki/Longest_palindromic_substring#Manacher's_algorithm
+
+function longestPalindrome(s: string) : string {
+
+
+
+}
+
+
