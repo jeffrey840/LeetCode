89920d3ff170ff9e88a5fcb69a7531b102a797df
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-12-19 00:18:29 -0600

    solved q 11

--- JavascriptsLeetcode/011.ContainerWithMostWater.js
+++ JavascriptsLeetcode/011.ContainerWithMostWater.js
@@ -25,3 +25,34 @@
 // 	n == height.length
 // 2 <= n <= 105
 // 0 <= height[i] <= 104
+
+//output a nuumber
+//counts every index in the array
+//keeps track of every index in the array
+
+let maxArea = function(height) {
+	let biggestFoundArea = 0;
+	let left = 0;
+	let right = height.length - 1;
+
+	// Run until the left and right markers meet
+	while (left < right) {
+		// Set the maximum height of the current area
+		const maxHeight = Math.min(height[left], height[right]);
+
+		// Calculate the current area
+		const currentArea = maxHeight * (right - left);
+
+		// Update the biggest found area
+		biggestFoundArea = Math.max(biggestFoundArea, currentArea);
+
+		// Move the left or right marker inwards (whichever is smaller)
+		if (height[left] < height[right]) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+
+	return biggestFoundArea;
+};
