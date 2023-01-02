6676f31393524624a55a533381124ec11ea06d7a
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-01 23:16:20 -0600

    solved #42

--- JavascriptGrind70/042.trappingRainWater.js
+++ JavascriptGrind70/042.trappingRainWater.js
@@ -20,6 +20,33 @@
 // 1 <= n <= 2 * 104
 // 0 <= height[i] <= 105
 
-var trap = function(height) {
 
+//calculate the max left position
+
+var trap = function(height) {
+	let sum = 0
+
+	let i = 0
+	let j = height.length - 1
+
+	while(i < j) {
+		if (height[i] <= height[j]) {
+			let k = i
+			while(height[k] <= height[i]) {
+				sum += height[i] - height[k]
+				k += 1
+			}
+			i = k
+		} else {
+			let k = j
+			while(height[k] <= height[j]) {
+				sum += height[j] - height[k]
+				k -= 1
+			}
+			j = k
+		}
+	}
+
+	return sum
 };
+
