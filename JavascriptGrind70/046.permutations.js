d1e9177aa8e7fd108562f0a79d56816aa40264da
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-03 22:22:31 -0600

    solved #46

--- JavascriptGrind70/046.permutations.js
+++ JavascriptGrind70/046.permutations.js
@@ -22,6 +22,24 @@
 // 	-10 <= nums[i] <= 10
 // All the integers of nums are unique.
 
+//return all posible combination of mubers in any order
+
 var permute = function(nums) {
+	const res = [];
+
+	const perm=(arr)=> {
+		if (arr.length === nums.length) {
+			res.push([...arr]);
+			return;
+		}
+		for(let i = 0;i < nums.length;i++) {
+			if (arr.includes(nums[i])) continue;
+			arr.push(nums[i]);
+			perm(arr);
+			arr.pop(nums[i])
+		}
+	}
+perm([]);
+	return res;
 
 };
