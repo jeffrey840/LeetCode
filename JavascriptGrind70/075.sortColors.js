e7ade9bd83872e6926891a24c4e53c647b3a9766
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-04 23:06:12 -0600

    solved #75

--- JavascriptGrind70/075.sortColors.js
+++ JavascriptGrind70/075.sortColors.js
@@ -22,6 +22,36 @@
 // 1 <= n <= 300
 // nums[i] is either 0, 1, or 2.
 
+// 0=red
+// 1=white
+// 2=blue
+
+//move all the 1 to fromt move all 2 to the back
+
 var sortColors = function(nums) {
 
+	if(nums.length === 0 || nums.length === 1) return;
+
+	let start = 0;//left most index
+	let end = nums.length -1;//right most index
+	let index = 0//index to loop
+
+	while(index <= end && start < end){
+
+		if(nums[index] === 0) {
+			nums[index] = nums[start];
+			nums[start] = 0;
+			start++;
+			index++;
+		//	pushing 2 to the back
+		} else if (nums[index] === 2) {
+			nums[index] = nums[end];
+			nums[end] = 2;
+			end--;
+		} else {
+			index++;
+		}
+	}
 };
+
+
