f0515de14583f7fbb5b92ba3c87cd8eccdb4eacb
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-14 01:35:22 -0600

    completed #76

--- JavascriptGrind70/076.minimumWindowSubstring.js
+++ JavascriptGrind70/076.minimumWindowSubstring.js
@@ -31,6 +31,61 @@
 // 1 <= m, n <= 105
 // s and t consist of uppercase and lowercase English letters.
 
+/*
+
+frequency map wil hold all the values of t
+
+check to see  if the value that we are currently in matches the t value
+
+when the frequency map equals 0 we will know that we found a substring within s that equals t
+
+
+* */
+
+
+
+
+
 var minWindow = function(s, t) {
 
+	let map = new map();
+
+	for (let letter of t) {
+		if(!map.has(letter)) {
+			map.set(letter,1);
+		} else {
+			map.set(letter, map.get(letter) + 1);
+		}
+	}
+
+	let left = 0;
+	let right = 0;
+	let len = Infinity;
+	let count = map.size;
+	let minWindow = "";
+
+	while (right < s.length) {
+
+		let rLetter = s[right];
+		if (map.has(rLetter)) {
+			map.set(rLetter, map.get(rLetter) -1);
+			if (map.get(rLetter) === 0) count --;
+		}
+		right++;
+
+		while (count === 0) {
+			if (right - left < len) {
+				len = right - left;
+				minWindow = s.slice(left,right);
+			}
+
+			let lLetter = s[left];
+			if (map.has(lLetter)) {
+				map.set(lLetter, map.get(lLetter) +1);
+				if (map.get(lLetter) > 0) count ++;
+			}
+			left++;
+		}
+	}
+	return minWindow;
 };
