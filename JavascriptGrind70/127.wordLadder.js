cb4ec4173c0c813b8ad8c7fb4cab3e5e70502113
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-15 17:33:49 -0600

    solved #127

--- JavascriptGrind70/127.wordLadder.js
+++ JavascriptGrind70/127.wordLadder.js
@@ -29,7 +29,52 @@
 // 	beginWord != endWord
 // All the words in wordList are unique.
 
-var ladderLength = function(beginWord, endWord, wordList) {
+// Return 0 if there is no such transformation sequence.
+// 	All words have the same length.
+// 	All words contain only lowercase alphabetic characters.
+// 	You may assume no duplicates in the word list.
+// 	You may assume beginWord and endWord are non-empty and are not the same.
 
+
+var ladderLength = function(beginWord, endWord, wordList) {
+	var wordSet = new Set(wordList);
+	var queue = [];
+	var step = 0;
+	var word = '';
+	var len = 0;
+	var i = 0;
+
+	pushNextWord(beginWord, queue, wordSet);
+	step = 2;
+
+	while (len = queue.length) {
+		for (i = 0; i < len; i++) {
+			word = queue.shift();
+			if (word === endWord) return step;
+			pushNextWord(word, queue, wordSet);
+		}
+		step++;
+	}
+
+	return 0;
+};
+
+var pushNextWord = function (word, queue, wordSet) {
+	var start = 'a'.charCodeAt(0);
+	var len = word.length;
+	var str = '';
+
+	wordSet.delete(word);
+
+	for (var i = 0; i < len; i++) {
+		for (var j = 0; j < 26; j++) {
+			str = word.substr(0, i) + String.fromCharCode(j + start) + word.substr(i + 1);
+
+			if (wordSet.has(str)) {
+				queue.push(str);
+				wordSet.delete(str);
+			}
+		}
+	}
 };
 
