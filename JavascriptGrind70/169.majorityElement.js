df57170cd2303c68b9891db717f7d305698653b0
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-29 22:46:26 -0600

    solved # 169

--- JavascriptGrind70/169.majorityElement.js
+++ JavascriptGrind70/169.majorityElement.js
@@ -13,6 +13,32 @@
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
 
+// retrun the highest num
+
+// hashmap is used to keep track of the number of ccurances
+
+
 var majorityElement = function(nums) {
 
+	let obj = {};
+	let count = 0;
+	let max = 0;
+
+	//this is the hashmap
+	for(let i of nums) {
+		if (!obj[i]) {
+			obj[i] = 1
+		} else {
+			obj[i]++
+		}
+	}
+
+	for(let j in obj) {
+		if(obj[j] > count) {
+			count  = obj[j]
+			max = j
+		}
+	}
+return max
+	// console.log(obj)
 };
