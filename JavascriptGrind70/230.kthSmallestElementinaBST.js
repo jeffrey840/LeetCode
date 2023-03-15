df13049fbcdcb83b3fed7f2fa1640065078cf4dc
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-03-14 23:47:55 -0500

    solved leetcode #230

--- JavascriptGrind70/230.kthSmallestElementinaBST.js
+++ JavascriptGrind70/230.kthSmallestElementinaBST.js
@@ -26,4 +26,23 @@
 
 var kthSmallest = function(root, k) {
 
+	let arr = [];
+	inOrder(root,arr);
+	return findKth(arr,k);
 };
+
+
+function inOrder(root, arr) {
+	if(!root) return;
+
+	inOrder(root.left,arr);
+	arr.push(root.val);
+	inOrder(root.right, arr);
+}
+
+function findKth (arr,k){
+
+	for(let i = 0;i < arr.length;i++) {
+		if(i === k -1) return arr[i];
+	}
+}
