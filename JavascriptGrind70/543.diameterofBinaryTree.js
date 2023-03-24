e817e7129b534c4309adf5d14df9b300c4208ef7
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-03-23 22:08:35 -0500

    solved leetcode #543

--- JavascriptGrind70/543.diameterofBinaryTree.js
+++ JavascriptGrind70/543.diameterofBinaryTree.js
@@ -17,6 +17,40 @@
 // Input: root = [1,2]
 // Output: 1
 
-var diameterOfBinaryTree = function(root) {
 
-};
+//We define a TreeNode class to represent the nodes of the binary tree. Each node has a val property to store its value, a left property to store its left child, and a right property to store its right child.
+class TreeNode {
+	constructor(val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+// We define a function diameterOfBinaryTree that takes the root node of the binary tree as input and returns the diameter of the binary tree.
+
+function diameterOfBinaryTree(root) {
+
+	// We initialize the diameter variable to 0. This variable will store the maximum diameter seen so far.
+	let diameter = 0;
+
+	// We define a helper function height that takes a node as input and returns its height. The height of a node is the maximum of the heights of its left and right children, plus 1. In addition, we update the diameter variable to store the maximum diameter seen so far, which is the sum of the heights of the left and right children.
+	function height(node) {
+		if (node === null) {
+			return 0;
+		}
+		const leftHeight = height(node.left);
+		const rightHeight = height(node.right);
+		diameter = Math.max(diameter, leftHeight + rightHeight);
+		return 1 + Math.max(leftHeight, rightHeight);
+	}
+
+	// We call the height function with the root node of the binary tree to compute the height of the binary tree and update the diameter variable.
+	height(root);
+
+	// We return the diameter variable as the output of the diameterOfBinaryTree function.
+	return diameter;
+}
+
+
+
