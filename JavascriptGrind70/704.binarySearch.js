36329cdf26a32f48603c0d48a6f7a2b144227fd9
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-03-24 10:52:06 -0500

    solved leetcode #704

--- JavascriptGrind70/704.binarySearch.js
+++ JavascriptGrind70/704.binarySearch.js
@@ -15,6 +15,32 @@
 // Output: -1
 // Explanation: 2 does not exist in nums so return -1
 
-var search = function(nums, target) {
 
-};
+//Initialize two pointers left and right to the first and last elements of the array, respectively.
+
+// While the left pointer is less than or equal to the right pointer:
+// Find the middle index of the search space by computing the average of left and right: middle = (left + right) // 2
+// Compare the target with the middle element nums[middle]:
+// If they are equal, return the middle index.
+
+// If target is greater than nums[middle], the target must be in the right half of the search space, so set left = middle + 1.
+// If target is less than nums[middle], the target must be in the left half of the search space, so set right = middle - 1.
+// If the target is not found after the while loop, return -1.
+
+function search(nums, target) {
+	let left = 0;
+	let right = nums.length - 1;
+
+	while (left <= right) {
+		const middle = Math.floor((left + right) / 2);
+		if (nums[middle] === target) {
+			return middle;
+		} else if (nums[middle] < target) {
+			left = middle + 1;
+		} else {
+			right = middle - 1;
+		}
+	}
+
+	return -1;
+}
