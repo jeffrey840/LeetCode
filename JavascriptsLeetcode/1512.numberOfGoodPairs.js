7ef0b98176b6dc07c6e03c5f7a955b99f579736e
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-10-17 22:03:20 -0500

    solved leetcode problem

---
+++ JavascriptsLeetcode/1512.numberOfGoodPairs.js
@@ -0,0 +1,17 @@
+function numIdenticalPairs(nums) {
+	let count = 0;// count is initialized to zero. This variable will keep track of the number of good pairs.
+	let frequencyMap = {};//This will store the frequency of each number we encounter.
+
+	for (let num of nums) {//For every num in nums, we'll do the following:
+
+		//If the number is already in frequencyMap
+		if (frequencyMap[num]) {
+			count += frequencyMap[num];//We increment our count by the frequency of that number.
+			frequencyMap[num]++;//then increment the frequency of that number in the frequencyMap
+		} else {//If the number isn't in frequencyMap
+			frequencyMap[num] = 1;//add it with a value of 1, indicating that we've seen it once.
+		}
+	}
+
+	return count;
+}
