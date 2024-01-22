52138eee648792801eb020ae333bb84d7b80f261
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-01-21 20:37:33 -0600

    solved leetcode question

---
+++ numOfSteps.java
@@ -0,0 +1,23 @@
+public class numOfSteps {
+
+    int numberOfSteps(int num) {
+
+
+        int steps = 0;
+
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+
+}
+
+
+
