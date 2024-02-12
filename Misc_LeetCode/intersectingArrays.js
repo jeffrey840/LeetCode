1f2fa13f078c7d899d513bb4cf64608b980fada3
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-02-11 23:39:37 -0600

    deleted comments

--- Misc_LeetCode/intersectingArrays.js
+++ Misc_LeetCode/intersectingArrays.js
@@ -1,10 +1,6 @@
 
 
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
+
 var intersection = function(nums1, nums2) {
 
 // check both arrays for repeating nums
