51684b8d081ccce2a70766298495c4858bebf856
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-02-12 15:36:15 -0600

    deleted old java files

--- Misc_LeetCode/intersectingArrays.js
+++ Misc_LeetCode/intersectingArrays.js
@@ -3,11 +3,6 @@
 
 var intersection = function(nums1, nums2) {
 
-// check both arrays for repeating nums
-// return array
-
-
-
 	for(let i = 0;i <= nums1.length;i++) {
 		const set1 = new Set(nums1);
 		const set2 = new Set(nums2);
