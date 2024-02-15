086bc8f787635bfbd281805947544825a6db57b2
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-02-15 15:59:23 -0600

    solved leetcode problem

---
+++ Misc_LeetCode/961.repeatedNTimes.js
@@ -0,0 +1,11 @@
+function repeatedNTimes(nums) {
+	const counts = {};
+	for (let num of nums) {
+		counts[num] = (counts[num] || 0) + 1;
+		if (counts[num] === nums.length / 2) {
+			return num;
+		}
+	}
+
+	return null;
+}
