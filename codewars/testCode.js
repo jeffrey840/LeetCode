7acaad4ff5190b3af9edd88d07b84880619cac89
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-02-24 23:55:46 -0600

    commiting progress

--- codewars/testCode.js
+++ codewars/testCode.js
@@ -1,11 +1,29 @@
 
 
-function gooseFilter (birds) {
-	var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-
-	return birds.filter(bird => !geese.includes(bird));
-
-		};
+function warnTheSheep(queue) {
 
 
-console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]))
+
+	// console.log(queue.indexOf("wolf"))
+	if(queue.indexOf("wolf") === queue.length - 1) {
+		console.log("Pls go away and stop eating my sheep")
+	}
+
+
+
+	let sheepIndanger = queue.indexOf("wolf") + 1
+	let myPov = queue.length - sheepIndanger
+
+	return "Oi! Sheep number "  + myPov + "! You are about to be eaten by a wolf!"
+
+
+}
+
+
+// console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]))
+//
+// console.log(warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"],))
+//
+// console.log(warnTheSheep(["sheep", "sheep", "wolf"]))
+
+console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"]))
