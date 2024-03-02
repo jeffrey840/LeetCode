28d1c8bad1e6ec243c3361fa4946858fa2b9c07e
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-03-02 00:06:01 -0600

    solved 1 codewars question

---
+++ codewars/findunqNUmber.js
@@ -0,0 +1,23 @@
+function findUniq(arr) {
+
+
+	if (arr[0] !== arr[1]) {
+		if (arr[0] !== arr[2]) {
+			return arr[0];
+		} else {
+			return arr[1];
+		}
+	}
+
+	// Iterate through the array starting from the third element
+	for (let i = 2; i < arr.length; i++) {
+		if (arr[i] !== arr[0]) {
+			return arr[i];
+		}
+	}
+
+}
+
+console.log(findUniq([ 0, 0, 0.55, 0, 0 ]) )
+console.log(findUniq([ 1, 0, 0 ]))
+console.log(findUniq([ 0, 1, 0 ]))
