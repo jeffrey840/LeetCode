27d25dd17f94290bc3c0548f79b27c548bfa84cb
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-03-07 22:40:15 -0600

    solved leetcode problem 169

---
+++ Misc_LeetCode/169.majorityElement.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+
+// for ever  number is ciunted they will be in storage
+
+
+	let count = 0;
+	let candidate = null;
+
+	for (let num of nums) {
+		if (count === 0) {
+			candidate = num;
+		}
+		count += (num === candidate) ? 1 : -1;
+	}
+
+	// The candidate is the majority element
+	return candidate;
+
+
+
+};
