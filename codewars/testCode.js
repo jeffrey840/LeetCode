eec28ffe36cd2e4e5d21839060e3432240b713fe
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-03-08 23:59:57 -0600

    solved 1 codewars question

---
+++ codewars/testCode.js
@@ -0,0 +1,19 @@
+function checkExam(array1, array2) {
+
+let examTotal = 16
+	let by4 = 0
+for(let i = 0;i <=array2.length;i++) {
+
+	if(array2[i].includes("")) {
+		by4 = 4
+	}
+
+
+
+}
+
+
+}
+
+
+console.log(checkExam(["a","b","c"],["a","",""]))
