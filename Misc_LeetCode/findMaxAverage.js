f47a6b88a3b2b6017d784c8aa3d273ab48dd78ef
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-03-15 17:08:42 -0500

    solved 1 leetcode problem

---
+++ Misc_LeetCode/findMaxAverage.js
@@ -0,0 +1,34 @@
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+
+	let sum = 0;
+	let maxSum = 0;
+
+	// Calculate the sum of the first k elements
+	for (let i = 0; i < k; i++) {
+		sum += nums[i];
+	}
+
+	// This is the max sum for the first window
+	maxSum = sum;
+
+	// Start from the kth element and slide the window to the right
+	for (let i = k; i < nums.length; i++) {
+		// Add the current element and remove the leftmost element from the sum
+		sum = sum + nums[i] - nums[i - k];
+
+		// Update maxSum if the current sum is greater
+		maxSum = Math.max(maxSum, sum);
+	}
+
+	// Return the maximum average
+	return maxSum / k;
+
+};
+
