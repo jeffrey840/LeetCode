f9a11db73cc46b75d7c250e736967cd024d5a3c7
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-03-14 21:16:37 -0500

    solved 1 leetcode problem

---
+++ Misc_LeetCode/duplicateZeros.js
@@ -0,0 +1,12 @@
+
+var duplicateZeros = function(arr) {
+	let i = 0;
+	while (i < arr.length) {
+		if (arr[i] === 0) {
+			arr.splice(i, 0, 0); // Insert 0 at the current index
+			arr.pop(); // Remove the last element to maintain array size
+			i += 1; // Skip the next zero since it's a duplicate
+		}
+		i += 1;
+	}
+};
