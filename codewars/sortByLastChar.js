4f31b1c94d7f33e8479ef4813b690587e23e7156
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-03-22 21:45:04 -0500

    solved 2 cw questions

---
+++ codewars/sortByLastChar.js
@@ -0,0 +1,20 @@
+function last(x){
+
+	let words = x.split(" ");
+	console.log(words)
+
+	// Sort the array based on the last character of each word
+	words.sort((a, b) => {
+		let lastCharA = a[a.length - 1];
+		let lastCharB = b[b.length - 1];
+		return lastCharA.localeCompare(lastCharB);
+	});
+
+	// Return the sorted array
+	return words;
+
+
+
+
+
+}
