c227e13eb30e81c5ce4f11c578e8b96d5a31ec93
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-04-01 17:38:26 -0500

    solved 1 lc question

---
+++ Misc_LeetCode/asteroidCollision.js
@@ -0,0 +1,39 @@
+function asteroidCollision(asteroids) {
+	const stack = [];
+
+	for (let asteroid of asteroids) {
+		console.log("Considering asteroid:", asteroid);
+
+		// Asteroid moving right: always add it to the stack
+		if (asteroid > 0) {
+			console.log("Right-moving asteroid, adding to stack.");
+			stack.push(asteroid);
+			continue;
+		}
+
+		// Asteroid moving left - potential collisions!
+		console.log("Left-moving asteroid, checking for collisions.");
+		while (stack.length > 0 && stack[stack.length - 1] > 0 && -asteroid > stack[stack.length - 1]) {
+			const explodedAsteroid = stack.pop();
+			console.log(
+				`Collision! Smaller right-moving asteroid (size ${explodedAsteroid}) explodes.`
+			);
+		}
+
+		// Check for other collision scenarios:
+		if (stack.length > 0 && stack[stack.length - 1] === -asteroid) {
+			stack.pop();
+			console.log(`Both asteroids explode (equal size: ${asteroid})`);
+		} else if (stack.length === 0 || stack[stack.length - 1] < 0) {
+			console.log("Left-moving asteroid survives, adding to stack.");
+			stack.push(asteroid);
+		}
+	}
+
+	console.log("Final surviving asteroids:", stack);
+	return stack;
+}
+
+// Example run
+const asteroids = [5, 10, -5];
+asteroidCollision(asteroids);
