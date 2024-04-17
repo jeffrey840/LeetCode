79434eaa6d14c863b8918c0f0449ddec39b1a09d
jeffrey cabrera <106411997+jeffrey840@users.noreply.github.com>
2024-04-16 20:36:32 -0500

    Time: 53 ms, Memory: 49.3 MB - LeetHub

---
+++ counting-elements/counting-elements.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var countElements = function(arr) {
+    counter = 0
+    let hashMap = new Set(arr)
+    for(let i = 0;i<=arr.length;i++) {
+        
+        let value = arr[i] + 1; 
+        
+        if(hashMap.has(value)) {
+            counter +=1
+        }
+        
+    }
+    return counter
+    
+    
+};
