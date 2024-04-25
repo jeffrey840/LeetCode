3e433666838ba134c8f25cb790c40b6356818ebd
jeffrey cabrera <106411997+jeffrey840@users.noreply.github.com>
2024-04-25 18:08:04 -0500

    Time: 300 ms, Memory: 114 MB - LeetHub

---
+++ design-hashset/design-hashset.js
@@ -0,0 +1,37 @@
+
+var MyHashSet = function() {
+    this.keyRange = 1000001;
+    this.set = new Array(this.keyRange);
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    this.set[key] = true;
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    this.set[key] = false;
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return !!this.set[key] 
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
