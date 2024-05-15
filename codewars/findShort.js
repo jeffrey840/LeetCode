6b72e2558179012c16a0b3462aabdfa78c5cbb80
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-05-15 12:12:12 -0500

    solved 2 cw questions

---
+++ codewars/findShort.js
@@ -0,0 +1,14 @@
+function findShort(s){
+
+	let words = s.split(" ");
+	let minLength = Infinity;
+
+	for(let word of words) {
+		if(word.length < minLength) {
+			minLength = word.length;
+		}
+	}
+	return minLength
+
+
+}
