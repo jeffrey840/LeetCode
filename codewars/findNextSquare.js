7b3864e3edfedb435c1639c1304acc3536ab9f1c
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-06-02 22:38:24 -0500

    solved 1 cw problem

---
+++ codewars/findNextSquare.js
@@ -0,0 +1,10 @@
+function findNextSquare(sq) {
+	// Return the next square if sq is a perfect square, -1 otherwise
+	const root = Math.sqrt(sq);
+
+	if(root % 1 === 0) {
+		return Math.pow(root + 1, 2);
+	}
+	return -1;
+
+}
