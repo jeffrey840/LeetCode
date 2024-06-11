c7c5188f765a8664e4196c1b9073e7dbf48dcc7f
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-06-11 17:33:59 -0500

    solved 1 cw problem

---
+++ codewars/symmetricPoint.js
@@ -0,0 +1,9 @@
+function symmetricPoint(P, Q) {
+	const Px = P[0];
+	const Py = P[1];
+	const Qx = Q[0];
+	const Qy = Q[1];
+	const P1x = 2 * Qx - Px;
+	const P1y = 2 * Qy - Py;
+	return [P1x, P1y];
+}
