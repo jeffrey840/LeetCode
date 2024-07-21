320b6b78cab6d6bb4b65913fda6f1e7226adcc7a
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-07-21 15:27:55 -0500

    solved 1 cw question

---
+++ codeSignal/sliding.js
@@ -0,0 +1,29 @@
+function sliding(nums, k) {
+	//code me
+	if(nums.length === 0) {
+		return [];
+	}
+
+	const result = [];
+	const deq = [];
+
+	for (let i = 0; i < nums.length; i++) {
+
+		while (deq.length && nums[deq[deq.length - 1]] <= nums[i]) {
+			deq.pop();
+		}
+
+		deq.push(i);
+
+		if (deq[0] <= i - k) {
+			deq.shift();
+		}
+
+		if (i >= k - 1) {
+			result.push(nums[deq[0]]);
+		}
+	}
+
+	return result;
+
+}
