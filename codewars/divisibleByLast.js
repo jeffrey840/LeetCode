3325167ae2819fa537250535cf0fb043ec459f08
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-08-07 19:35:44 -0500

    solved 1 cw problem

---
+++ codewars/divisibleByLast.js
@@ -0,0 +1,13 @@
+function divisibleByLast(n) {
+	// TODO
+	let str = n.toString().split('')
+	const arr = [false];
+	for(let i = 1; i < str.length; ++i){
+		if(str[i] % str[i-1] === 0){
+			arr.push(true);
+		}else{
+			arr.push(false);
+		};
+	};
+	return arr;
+}
