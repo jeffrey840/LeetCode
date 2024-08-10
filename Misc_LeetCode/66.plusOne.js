2912a6efccb725d9b34373693c3fada165934846
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-08-09 22:17:02 -0500

    solved 2 lc problems

---
+++ Misc_LeetCode/66.plusOne.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+	for (let i = digits.length - 1; i >= 0; i--) {
+		if (digits[i] < 9) {
+			digits[i]++;
+			return digits;
+		}
+		digits[i] = 0;
+	}
+
+	// If all digits were 9, we end up here, so we need to add a 1 at the beginning
+	digits.unshift(1);
+	return digits;
+};
