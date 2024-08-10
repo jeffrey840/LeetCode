b2f7721812db5e4c766bdfafabcff8ac029553e0
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-08-10 15:20:19 -0500

    added comments

--- Misc_LeetCode/66.plusOne.js
+++ Misc_LeetCode/66.plusOne.js
@@ -11,6 +11,10 @@ var plusOne = function(digits) {
 		digits[i] = 0;
 	}
 
+
+
+
+
 	// If all digits were 9, we end up here, so we need to add a 1 at the beginning
 	digits.unshift(1);
 	return digits;
