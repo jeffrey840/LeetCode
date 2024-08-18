e1dd42952ca1281f3e871d083e3778dbbbf1feee
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-08-18 18:33:24 -0500

    solved 1 cw problem

---
+++ codewars/sumOrProduct.js
@@ -0,0 +1,19 @@
+function sumOrProduct(array, n) {
+	// Sort the array in ascending order
+	array.sort((a, b) => a - b);
+
+	// Calculate the sum of the n largest numbers
+	const sumLargest = array.slice(-n).reduce((sum, num) => sum + num, 0);
+
+	// Calculate the product of the n smallest numbers
+	const productSmallest = array.slice(0, n).reduce((product, num) => product * num, 1);
+
+	// Compare the two results
+	if (sumLargest > productSmallest) {
+		return "sum";
+	} else if (productSmallest > sumLargest) {
+		return "product";
+	} else {
+		return "same";
+	}
+}
