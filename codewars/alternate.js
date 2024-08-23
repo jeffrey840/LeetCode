e0d28ff00e24251e6651aedd0f3753737d20ce05
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2024-08-22 19:34:46 -0500

    solved 1 cw problem

---
+++ codewars/alternate.js
@@ -0,0 +1,14 @@
+function alternate(items) {
+	// Handle the case of an empty list
+	if (items.length === 0) return null;
+
+	// Iterate through the list and perform the OR operation
+	for (let i = 0; i < items.length; i++) {
+		if (items[i]) {
+			return true;
+		}
+	}
+
+	// If no true value was found, return false
+	return false;
+}
